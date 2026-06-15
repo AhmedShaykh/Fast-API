71a9efeea325f36ec030ef630277ef4c90a04dd6
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2026-06-15 14:48:10 +0500

    Final Commit

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -4,9 +4,9 @@ from typing import Optional;
 class ProductSchema(BaseModel):
     title: str;
     desc: str;
-    price: float;
+    complete: bool = False;
 
 class UpdateProductSchema(BaseModel):
     title: Optional[str] = None;
     desc: Optional[str] = None;
-    price: Optional[float] = None;
+    complete: Optional[bool] = None;
